59b6bf7bd1034fdcb0c934c75c0d904e0e52b388
suyash1798 <suyashtiwari1798@gmail.com>
2018-08-05 19:27:16 +0530

    database added

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,12 @@ const mongoose = require('mongoose');
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://suyash1798:suyash1@ds113452.mlab.com:13452/authregistration',{ useNewUrlParser: true } ,(err)=>{
+    if(err){
+    console.log(err);
+}
+});
+
 const port = 3000;
 
 var posts = [
@@ -14,17 +20,31 @@ var posts = [
     {message:'what going on'}
 ]
 
+var User = require('./models/user');
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
 app.post('/register',(req,res)=>{
    let userData = req.body;
+   let user = new User(userData);
+
+   user.save((err,result)=>{
+       console.log("its done");
+       if(err){
+           console.log("Having trouble Adding User");
+           res.send(err);
+       }else{
+
+           res.sendStatus(200);
+       }
+   })
    console.log(userData.email);
    res.sendStatus(200);
 });
 
-mongoose.connect('mongodb://suyash:suyash@1798@ds113442.mlab.com:13442/meanstackauth',(err)=>{
+mongoose.connect('mongodb://suyash:suyash1234@ds113442.mlab.com:13442/meanstackauth',{ useNewUrlParser: true } ,(err)=>{
     if(!err){
         console.log('Connected to mongo Database');
 }
